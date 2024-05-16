cb33c4d59eb4cf630cf587811a4ac70727e37349
Nino Wyssmann <nino.wyssmann@student.unibe.ch>
2024-05-16 11:24:18 +0200

    Bug fixed, CSV file not found

--- src/load_and_fix_data.py
+++ src/load_and_fix_data.py
@@ -8,7 +8,7 @@ class LoadAndFixData:
     a class to fix raw data in the csv file
     """
     def __init__(self):
-        self.file_path = 'data/Airbnb_Open_Data.csv'
+        self.file_path = '../data/Airbnb_Open_Data.csv'
 
     def load_and_fix_data(self):
         """
